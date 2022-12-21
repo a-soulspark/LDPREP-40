12481ee5f772052f5160987fabd5a76a9cd38715
a-soulspark <pedrobbcicatriz@gmail.com>
2022-12-21 18:26:01 +0000

    Slime throwin'

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 export var SPEED: int = 10000
 export var walk_animation_name = "walk"
 export var idle_animation_name = "idle"
-onready var animations: AnimatedSprite =  $Animations
+onready var animations: AnimatedSprite = $Animations
 
 func _input(event):
 	pass
@@ -27,10 +27,21 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		spawn_slime()
 	
-	if velocity.x == 0:
-		animations.play(idle_animation_name)
+	if Input.is_action_just_pressed("throw"):
+		throw_slime()
+	
+	if animations.animation == walk_animation_name:
+		var progress = float(animations.frame) / animations.frames.get_frame_count(animations.animation)
+		animations.position.y = -abs(sin(progress * PI)) * 10
+	
+	print(animations.animation)
+	if velocity.length() == 0:
+		if animations.animation == walk_animation_name and animations.frame == 0:
+			animations.play(idle_animation_name)
 	else:
-		animations.play(walk_animation_name)
+		if animations.animation == idle_animation_name:
+			animations.frame = 0
+			animations.play(walk_animation_name)
 	
 	velocity = velocity.normalized() * SPEED * delta
 	
@@ -39,14 +50,33 @@ func _physics_process(delta):
 
 export var slime_scene: PackedScene = null
 var slime_list = []
+var throwable_slimes = []
 
 func spawn_slime():
 	var slime = slime_scene.instance()
 	slime_list.append(slime)
+	throwable_slimes.append(slime)
+
 	slime.position = position
 	slime.player = self
 	slime.follow_delay *= len(slime_list)
 	get_parent().add_child(slime)
 
+func throw_slime():
+	if len(throwable_slimes) == 0: return
+	
+	var slime_to_throw = throwable_slimes.pop_back()
+	
+	slime_to_throw.throw(get_global_mouse_position())
+	slime_to_throw.connect("hit", self, "_on_slime_hit", [slime_to_throw], CONNECT_ONESHOT)
+
+func _on_slime_hit(slime):
+	throwable_slimes.append(slime)
+
 func _ready():
 	animations.play()
+
+func _on_animation_finished():
+	pass
+	#if animations.animation == walk_animation_name:
+#		animations.play(idle_animation_name)
--- Slime/SlimeFollow.gd
+++ Slime/SlimeFollow.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal hit
+
 class PositionSnapshot:
 	var time : float
 	var position : Vector2
@@ -10,19 +12,21 @@ class PositionSnapshot:
 
 export var player_path : NodePath
 export var follow_delay : float
+export var throw_duration : float = 1
 
 onready var player : KinematicBody2D = get_node(player_path)
 
 var positions : Array = []
 var target_position
+var throw_timer = 0
+var throw_start : Vector2
+var throw_target : Vector2
 
 func _ready():
 	add_collision_exception_with(player)
 	target_position = player.position
 
 func _process(delta):
-	if Input.is_action_pressed("ui_select"): return
-	
 	var current_time = Time.get_ticks_msec()
 	positions.append(PositionSnapshot.new(current_time, player.position))
 	
@@ -32,6 +36,20 @@ func _process(delta):
 			target_position = p.position
 		else:
 			break
-	position = lerp(position, target_position, 0.075)
-	look_at(target_position)
+	
+	if throw_timer > 0:
+		throw_timer -= delta
+		if throw_timer / throw_duration < 0.5:
+			throw_start = target_position
+		
+		position = lerp(throw_start, throw_target, ease(sin((1 - throw_timer / throw_duration) * PI), 0.75))
+		if throw_timer <= 0:
+			emit_signal("hit")
+	else:
+		position = lerp(position, target_position, 0.075)
+		look_at(target_position)
 
+func throw(target):
+	throw_timer = throw_duration
+	throw_start = position
+	throw_target = target
