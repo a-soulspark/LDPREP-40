9afe2d19674fb28d98cbc120203e904b9a262e8f
shnCanos <catellacatellacarettacaretta@gmail.com>
2022-12-22 17:03:04 +0000

    Added sounds

--- Menus/MainMenu/MainMenu.gd
+++ Menus/MainMenu/MainMenu.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
+onready var buttons_animation = $ButtonAnimations
 onready var title_animation = $TitleAnimation
+onready var hover_sound = $HoverSound
+onready var click_sound = $ClickSound
+
 func _ready():
 	title_animation.play("FloatingTitle")
 
 func _on_Start_pressed():
+	click_sound.play()
 	Transition.change_scene("res://Enemies/EnemyTest.tscn")
 
 
 func _on_Exit_pressed():
+	click_sound.play()
 	get_tree().quit()
 
 func _on_TitleAnimation_animation_finished(anim_name):
@@ -16,3 +22,27 @@ func _on_TitleAnimation_animation_finished(anim_name):
 		title_animation.play("FloatingTitleBackwards")
 	else:
 		title_animation.play("FloatingTitle")
+
+
+func _on_Start_mouse_entered():
+	buttons_animation.play("StartHoverAnimations")
+	hover_sound.play()
+
+
+
+func _on_Start_mouse_exited():
+	buttons_animation.play_backwards("StartHoverAnimations")
+
+
+func _on_Settings_mouse_entered():
+	hover_sound.play()
+
+
+func _on_Exit_mouse_entered():
+	hover_sound.play()
+
+
+func _on_Settings_pressed():
+	click_sound.play()
+
+
