71365d37912d3bad5516fbff6a6697de652ebe54
shnCanos <catellacatellacarettacaretta@gmail.com>
2022-12-22 12:16:50 +0000

    UwUed the main menu

--- Menus/Settings/Settings.gd
+++ Menus/Settings/Settings.gd
@@ -9,3 +9,19 @@ func appear():
 
 func change_position_to(position):
 	settings_container.rect_global_position = position
+
+
+func _on_CheckBox_item_selected(index):
+	if index == 0:
+		OS.window_borderless = false
+		OS.window_maximized = false
+		OS.window_resizable = true
+		OS.window_fullscreen = false
+	if index == 1:
+		OS.window_borderless = true
+		OS.window_maximized = true
+		OS.window_resizable = false	
+		OS.window_fullscreen = false
+	if index == 2:
+		OS.window_borderless = false
+		OS.window_fullscreen = true
