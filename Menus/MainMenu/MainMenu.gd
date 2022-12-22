40ec752ca1dbb89bada77b702462387055faae90
shnCanos <catellacatellacarettacaretta@gmail.com>
2022-12-22 14:36:36 +0000

    Improved menu

--- Menus/MainMenu/MainMenu.gd
+++ Menus/MainMenu/MainMenu.gd
@@ -11,11 +11,6 @@ func _on_Start_pressed():
 func _on_Exit_pressed():
 	get_tree().quit()
 
-export var settings_position: Vector2
-func _on_Options_pressed():
-	Settings.appear()
-	Settings.change_position_to(settings_position)
-
 func _on_TitleAnimation_animation_finished(anim_name):
 	if anim_name == "FloatingTitle":
 		title_animation.play("FloatingTitleBackwards")
